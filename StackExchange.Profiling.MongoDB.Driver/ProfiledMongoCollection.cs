fb1b262cf917906e67575eb63a210a3dfab383c8
Gabe Williamson <gabe.williamson@vminnovations.com>
2015-12-08 14:27:20 -0600

    Start wrapping the MongoCollection

---
+++ StackExchange.Profiling.MongoDB.Driver/ProfiledMongoCollection.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+
+namespace StackExchange.Profiling.MongoDB.Driver
+{
+    /// <summary>
+    /// TODO: Create a ContinueWithMiniProfiler extension method that starts a profiler and stops when the task completes. Can be done later. Need to do more research if possible.
+    /// </summary>
+    /// <typeparam name="TDocument"></typeparam>
+    public class ProfiledMongoCollection<TDocument> : IMongoCollection<TDocument>
+    {
+        private readonly IMongoCollection<TDocument> _collection;
+
+        public ProfiledMongoCollection(IMongoCollection<TDocument> CollectionToProfile ) {
+            this._collection = CollectionToProfile;
+        }
+
+        public CollectionNamespace CollectionNamespace
+        {
+            get
+            {
+                return _collection.CollectionNamespace;
+            }
+        }
+
+        public IMongoDatabase Database
+        {
+            get
+            {
+                return _collection.Database;
+            }
+        }
+
+        public IBsonSerializer<TDocument> DocumentSerializer
+        {
+            get
+            {
+                return _collection.DocumentSerializer;
+            }
+        }
+
+        public IMongoIndexManager<TDocument> Indexes
+        {
+            get
+            {
+                return _collection.Indexes;
+            }
+        }
+
+        public MongoCollectionSettings Settings
+        {
+            get
+            {
+                return _collection.Settings;
+            }
+        }
+
+        public IAsyncCursor<TResult> Aggregate<TResult>(PipelineDefinition<TDocument, TResult> pipeline, AggregateOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.Aggregate<TResult>(pipeline, options, cancellationToken);
+        }
+
+        public Task<IAsyncCursor<TResult>> AggregateAsync<TResult>(PipelineDefinition<TDocument, TResult> pipeline, AggregateOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.AggregateAsync<TResult>(pipeline, options, cancellationToken);
+        }
+
+        public BulkWriteResult<TDocument> BulkWrite(IEnumerable<WriteModel<TDocument>> requests, BulkWriteOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.BulkWrite(requests, options, cancellationToken);
+        }
+
+        public Task<BulkWriteResult<TDocument>> BulkWriteAsync(IEnumerable<WriteModel<TDocument>> requests, BulkWriteOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.BulkWriteAsync(requests, options, cancellationToken);
+        }
+
+        public long Count(FilterDefinition<TDocument> filter, CountOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.Count(filter, options, cancellationToken);
+        }
+
+        public Task<long> CountAsync(FilterDefinition<TDocument> filter, CountOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.CountAsync(filter, options, cancellationToken);
+        }
+
+        public DeleteResult DeleteMany(FilterDefinition<TDocument> filter, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.DeleteMany(filter, cancellationToken);
+        }
+
+        public Task<DeleteResult> DeleteManyAsync(FilterDefinition<TDocument> filter, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.DeleteManyAsync(filter, cancellationToken);
+        }
+
+        public DeleteResult DeleteOne(FilterDefinition<TDocument> filter, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.DeleteOne(filter, cancellationToken);
+        }
+
+        public Task<DeleteResult> DeleteOneAsync(FilterDefinition<TDocument> filter, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.DeleteOneAsync(filter, cancellationToken);
+        }
+
+        public IAsyncCursor<TField> Distinct<TField>(FieldDefinition<TDocument, TField> field, FilterDefinition<TDocument> filter, DistinctOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.Distinct<TField>(field, filter, options, cancellationToken);
+        }
+
+        public Task<IAsyncCursor<TField>> DistinctAsync<TField>(FieldDefinition<TDocument, TField> field, FilterDefinition<TDocument> filter, DistinctOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.DistinctAsync<TField>(field, filter, options, cancellationToken);
+        }
+
+        public Task<IAsyncCursor<TProjection>> FindAsync<TProjection>(FilterDefinition<TDocument> filter, FindOptions<TDocument, TProjection> options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.FindAsync<TProjection>(filter, options, cancellationToken);
+        }
+
+        public TProjection FindOneAndDelete<TProjection>(FilterDefinition<TDocument> filter, FindOneAndDeleteOptions<TDocument, TProjection> options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.FindOneAndDelete<TProjection>(filter, options, cancellationToken);
+        }
+
+        public Task<TProjection> FindOneAndDeleteAsync<TProjection>(FilterDefinition<TDocument> filter, FindOneAndDeleteOptions<TDocument, TProjection> options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.FindOneAndDeleteAsync<TProjection>(filter, options, cancellationToken);
+        }
+
+        public TProjection FindOneAndReplace<TProjection>(FilterDefinition<TDocument> filter, TDocument replacement, FindOneAndReplaceOptions<TDocument, TProjection> options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.FindOneAndReplace<TProjection>(filter, replacement, options, cancellationToken);
+        }
+
+        public Task<TProjection> FindOneAndReplaceAsync<TProjection>(FilterDefinition<TDocument> filter, TDocument replacement, FindOneAndReplaceOptions<TDocument, TProjection> options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.FindOneAndReplaceAsync<TProjection>(filter, replacement, options, cancellationToken);
+        }
+
+        public TProjection FindOneAndUpdate<TProjection>(FilterDefinition<TDocument> filter, UpdateDefinition<TDocument> update, FindOneAndUpdateOptions<TDocument, TProjection> options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.FindOneAndUpdate<TProjection>(filter, update, options, cancellationToken);
+        }
+
+        public Task<TProjection> FindOneAndUpdateAsync<TProjection>(FilterDefinition<TDocument> filter, UpdateDefinition<TDocument> update, FindOneAndUpdateOptions<TDocument, TProjection> options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.FindOneAndUpdateAsync<TProjection>(filter, update, options, cancellationToken);
+        }
+
+        public IAsyncCursor<TProjection> FindSync<TProjection>(FilterDefinition<TDocument> filter, FindOptions<TDocument, TProjection> options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.FindSync<TProjection>(filter, options, cancellationToken);
+        }
+
+        public void InsertMany(IEnumerable<TDocument> documents, InsertManyOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            _collection.InsertMany(documents, options, cancellationToken);
+        }
+
+        public Task InsertManyAsync(IEnumerable<TDocument> documents, InsertManyOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.InsertManyAsync(documents, options, cancellationToken);
+        }
+
+        public void InsertOne(TDocument document, InsertOneOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            _collection.InsertOne(document, options, cancellationToken);
+        }
+
+        public Task InsertOneAsync(TDocument document, CancellationToken _cancellationToken)
+        {
+            return _collection.InsertOneAsync(document, _cancellationToken);
+        }
+
+        public Task InsertOneAsync(TDocument document, InsertOneOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.InsertOneAsync(document, options, cancellationToken);
+        }
+
+        public IAsyncCursor<TResult> MapReduce<TResult>(BsonJavaScript map, BsonJavaScript reduce, MapReduceOptions<TDocument, TResult> options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.MapReduce<TResult>(map, reduce, options, cancellationToken);
+        }
+
+        public Task<IAsyncCursor<TResult>> MapReduceAsync<TResult>(BsonJavaScript map, BsonJavaScript reduce, MapReduceOptions<TDocument, TResult> options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.MapReduceAsync<TResult>(map, reduce, options, cancellationToken);
+        }
+
+        public IFilteredMongoCollection<TDerivedDocument> OfType<TDerivedDocument>() where TDerivedDocument : TDocument
+        {
+            return _collection.OfType<TDerivedDocument>();
+        }
+
+        public ReplaceOneResult ReplaceOne(FilterDefinition<TDocument> filter, TDocument replacement, UpdateOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.ReplaceOne(filter, replacement, options, cancellationToken);
+        }
+
+        public Task<ReplaceOneResult> ReplaceOneAsync(FilterDefinition<TDocument> filter, TDocument replacement, UpdateOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.ReplaceOneAsync(filter, replacement, options, cancellationToken);
+        }
+
+        public UpdateResult UpdateMany(FilterDefinition<TDocument> filter, UpdateDefinition<TDocument> update, UpdateOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.UpdateMany(filter, update, options, cancellationToken);
+        }
+
+        public Task<UpdateResult> UpdateManyAsync(FilterDefinition<TDocument> filter, UpdateDefinition<TDocument> update, UpdateOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.UpdateManyAsync(filter, update, options, cancellationToken);
+        }
+
+        public UpdateResult UpdateOne(FilterDefinition<TDocument> filter, UpdateDefinition<TDocument> update, UpdateOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.UpdateOne(filter, update, options, cancellationToken);
+        }
+
+        public Task<UpdateResult> UpdateOneAsync(FilterDefinition<TDocument> filter, UpdateDefinition<TDocument> update, UpdateOptions options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _collection.UpdateOneAsync(filter, update, options, cancellationToken);
+        }
+
+        public IMongoCollection<TDocument> WithReadConcern(ReadConcern readConcern)
+        {
+            return _collection.WithReadConcern(readConcern);
+        }
+
+        public IMongoCollection<TDocument> WithReadPreference(ReadPreference readPreference)
+        {
+            return _collection.WithReadPreference(readPreference);
+        }
+
+        public IMongoCollection<TDocument> WithWriteConcern(WriteConcern writeConcern)
+        {
+            return _collection.WithWriteConcern(writeConcern);
+        }
+    }
+}
